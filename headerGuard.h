eefbaf82b288c70de3bd67209c1d8857208c6891
fred1912 <fred1912@users.noreply.github.com>
2017-02-13 15:42:08 +1100

    Add files via upload

---
+++ headerGuard.h
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   headerGuard.h
+ * Author: fredzhao
+ *
+ * Created on 2017年2月13日, 下午3:41
+ */
+
+#ifndef HEADERGUARD_H
+#define HEADERGUARD_H
+class file1
+{
+    
+};
+
+
+#endif /* HEADERGUARD_H */
+
